9cdcec6a6557abe6cce619a416536ed22b7a643e
furqonstudio <furqonstudio@gmail.com>
2025-02-26 21:04:21 +0700

    feat: create models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['total_price', 'status'];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['order_id', 'payment_method', 'amount', 'payment_status'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
