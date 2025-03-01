e4bac73d287111fcad6e17e5166ee5b776c6b837
furqonstudio <furqonstudio@gmail.com>
2025-03-01 10:19:59 +0700

    feat: create failedValidation

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
@@ -16,17 +19,34 @@ class CategoryRequest extends FormRequest
         $id = $this->route('category')?->id;
 
         return [
-            'name' => 'required|string|max:255|unique:categories,name,' . ($id ?? 'NULL'),
+            'name' => [
+                'bail',
+                'required',
+                'string',
+                'min:3',
+                'max:255',
+                Rule::unique('categories', 'name')->ignore($id),
+            ],
         ];
     }
 
-    // public function messages(): array
-    // {
-    //     return [
-    //         'name.required' => 'Nama kategori wajib diisi.',
-    //         'name.string' => 'Nama kategori harus berupa teks.',
-    //         'name.max' => 'Nama kategori tidak boleh lebih dari 255 karakter.',
-    //         'name.unique' => 'Nama kategori sudah digunakan.',
-    //     ];
-    // }
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nama kategori wajib diisi.',
+            'name.string' => 'Nama kategori harus berupa teks.',
+            'name.min' => 'Nama kategori minimal 3 karakter.',
+            'name.max' => 'Nama kategori maksimal 255 karakter.',
+            'name.unique' => 'Nama kategori sudah digunakan.',
+        ];
+    }
+
+    function failedValidation(ValidationValidator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validasi gagal',
+            'errors' => $validator->errors()->first(),
+        ], 422));
+    }
 }
