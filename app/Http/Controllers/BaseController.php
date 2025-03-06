f983d833da3e9d02b66212956485d4dfc5e7cb61
furqonstudio <furqonstudio@gmail.com>
2025-03-06 22:31:59 +0700

    refactor: Refactor successResponse to handle PaginationResource correctly

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PaginationResource;
 use Illuminate\Http\JsonResponse;
 
 class BaseController extends Controller
 {
     protected function successResponse($data, string $message, int $status = 200): JsonResponse
     {
+        if ($data instanceof PaginationResource) {
+            return response()->json([
+                'success' => true,
+                'message' => $message,
+                'data' => $data->toArray(request())['data'],
+                'pagination' => $data->toArray(request())['pagination'],
+            ], $status);
+        }
+
         return response()->json([
             'success' => true,
             'message' => $message,
