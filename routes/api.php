c7fc9398d371496ef5562634a8bfbf190ef28b69
furqonstudio <furqonstudio@gmail.com>
2025-03-22 15:13:26 +0700

    feat: create stores route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,4 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
 Route::apiResource('orders', OrderController::class);
+Route::apiResource('stores', StoreController::class);
