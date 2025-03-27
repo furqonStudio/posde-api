58202c91302e0d8326c8a331132eef3ddb8d1c37
furqonstudio <furqonstudio@gmail.com>
2025-03-27 21:47:08 +0700

    feat: add product migration with user_id and image

--- database/migrations/2025_02_26_135508_create_products_table.php
+++ database/migrations/2025_02_26_135508_create_products_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('image')->nullable();
             $table->string('name');
             $table->decimal('price', 10, 2);
             $table->integer('stock');
