286f8e47225b13283425aaaf853582a4823f8208
furqonstudio <furqonstudio@gmail.com>
2025-03-28 13:25:09 +0700

    feat: update product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,7 +17,9 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            'category_id' => Category::inRandomOrder()->first()?->id ?? Category::factory(),
+            'category_id' => Category::query()->inRandomOrder()->value('id') ?? Category::factory(),
+            'user_id' => User::query()->inRandomOrder()->value('id') ?? User::factory(),
+            'image' => $this->faker->imageUrl(640, 480, 'products', true, 'Dummy Product'),
             'name' => $this->faker->sentence(2),
             'description' => $this->faker->paragraph,
             'price' => $this->faker->numberBetween(10000, 500000),
